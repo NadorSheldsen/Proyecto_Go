c58f8020045d2b4b55d8b292a7a71cbbcd9df50d
IxchelRmz <lunavaleria661@gmail.com>
2024-10-25 10:57:35 -0600

    Materias y Calificaciones

--- database/models.go
+++ database/models.go
@@ -11,7 +11,26 @@ type Alumnos struct {
 	Group      string
 	Email      string
 }
+type Calificacion struct {
+	gorm.Model
+	GradeID   int64   `gorm:"primaryKey"`
+	StudentID int64   `gorm:"not null"`
+	SubjectID int64   `gorm:"not null"`
+	Grade     float64 `gorm:"not null"`
+}
+type Materia struct {
+	gorm.Model
+	SubjectID int64  `gorm:"primaryKey"`
+	Name      string `gorm:"not null"`
+}
 
 func (Alumnos) TableName() string {
 	return "Alumnos"
+}
+func (Calificacion) TableName() string {
+	return "Calificaciones"
+}
+
+func (Materia) TableName() string {
+	return "Materias"
 }
